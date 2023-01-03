f9d621f7406422a93528772a0067b29a6d057595
Niko Wilhelm <niko.wilhelm@gmx.net>
2023-01-03 21:14:35 +0100

    Start implementing some rules

--- src/parsers/ast.rs
+++ src/parsers/ast.rs
@@ -1,27 +1,54 @@
+// <base-type> ::= <name>
 pub struct BaseType {
     pub name: String,
 }
 
+// <atomic-formula-skeleton> ::=
+//     (<predicate> <typed list (variable)>)
+pub struct AtomicFormulaSkeleton {
+    pub predicate: Predicate,
+    pub typed_list: TypedList<Variable>,
+}
+
+// <predicate> ::= <name>
 pub struct Predicate {
     pub name: String,
 }
 
+// <variable> ::= ?<name>
 pub struct Variable {
     pub name: String,
 }
 
+// <typed list (x)> ::= x+ - <type>
+//     [<typed list (x)>]
+pub struct TypedList<T> {
+    pub elems: Vec<(Vec<T>, Type)>,
+}
+
+// <primitive-type> ::= <name>
 pub struct PrimitiveType {
     pub name: String,
 }
 
+// <type> ::= (either <primitive-type>+)
+// <type> ::= <primitive-type>
+pub enum Type {
+    Simple(PrimitiveType),
+    Either(Vec<PrimitiveType>),
+}
+
+// <task-symbol> ::= <name>
 pub struct TaskSymbol {
     pub name: String,
 }
 
+// <subtask-id> ::= <name>
 pub struct SubtaskId {
     pub name: String,
 }
 
+// <term> ::= <name>
 pub struct Term {
     pub name: String,
 }
--- src/parsers/shared.rs
+++ src/parsers/shared.rs
@@ -1,6 +1,9 @@
 use chumsky::prelude::*;
 
-use super::ast::{BaseType, Predicate, PrimitiveType, SubtaskId, TaskSymbol, Term, Variable};
+use super::ast::{
+    AtomicFormulaSkeleton, BaseType, Predicate, PrimitiveType, SubtaskId, TaskSymbol, Term, Type,
+    TypedList, Variable,
+};
 
 pub fn parse_name() -> impl Parser<char, String, Error = Simple<char>> {
     text::ident::<_, Simple<char>>().padded()
@@ -9,6 +12,20 @@ pub fn parse_name() -> impl Parser<char, String, Error = Simple<char>> {
 // <base-type> ::= <name>
 pub fn parse_basetype() -> impl Parser<char, BaseType, Error = Simple<char>> {
     parse_name().map(|name| BaseType { name })
+}
+
+// <atomic-formula-skeleton> ::=
+//     (<predicate> <typed list (variable)>)
+pub fn parse_atomicformulaskeleton(
+) -> impl Parser<char, AtomicFormulaSkeleton, Error = Simple<char>> {
+    parse_predicate()
+        .padded()
+        .then(parse_typedlist(parse_variable()))
+        .delimited_by(just('(').padded(), just(')').padded())
+        .map(|(predicate, typed_list)| AtomicFormulaSkeleton {
+            predicate,
+            typed_list,
+        })
 }
 
 // <predicate> ::= <name>
@@ -20,12 +37,43 @@ pub fn parse_predicate() -> impl Parser<char, Predicate, Error = Simple<char>> {
 pub fn parse_variable() -> impl Parser<char, Variable, Error = Simple<char>> {
     just('?')
         .ignore_then(parse_name())
+        .padded()
         .map(|name| Variable { name })
+}
+
+// <typed list (x)> ::= x+ - <type>
+//     [<typed list (x)>]
+pub fn parse_typedlist<T>(
+    parser: impl Parser<char, T, Error = Simple<char>>,
+) -> impl Parser<char, TypedList<T>, Error = Simple<char>> {
+    parser
+        .padded()
+        .repeated()
+        .at_least(1)
+        .then(parse_type())
+        .repeated()
+        .at_least(1)
+        .map(|elems| TypedList { elems })
 }
 
 // <primitive-type> ::= <name>
 pub fn parse_primitivetype() -> impl Parser<char, PrimitiveType, Error = Simple<char>> {
     parse_name().map(|name| PrimitiveType { name })
+}
+
+// <type> ::= (either <primitive-type>+)
+// <type> ::= <primitive-type>
+pub fn parse_type() -> impl Parser<char, Type, Error = Simple<char>> {
+    parse_primitivetype()
+        .padded()
+        .repeated()
+        .at_least(1)
+        .delimited_by(
+            just('(').padded().then(just("either").padded()),
+            just(')').padded(),
+        )
+        .map(|types| Type::Either(types))
+        .or(parse_primitivetype().map(|ptype| Type::Simple(ptype)))
 }
 
 // <task-symbol> ::= <name>
